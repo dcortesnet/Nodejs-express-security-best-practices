c0aca7c3eeebdd87369e99e6af97d640df7250d8
dcortesnet <dcortes.net@gmail.com>
2023-06-28 13:02:10 -0400

    Add details keep-alive

--- keep-alive/index.js
+++ keep-alive/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+
 const server = app.listen(3000, () => {
   console.log('server on port 3000');
 });
-server.keepAliveTimeout = 30 * 1000;
-server.headersTimeout = 35 * 1000;
+
+server.keepAliveTimeout = 30 * 1000; // 30 seconds
+server.headersTimeout = 35 * 1000; // 35 seconds
